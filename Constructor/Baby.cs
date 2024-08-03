81d49fccf14ec1afe65e33bf02a494cc65daa2e5
Zeynep Tuana Çakır <2320711004@ogrenci.ibu.edu.tr>
2024-08-03 10:43:40 +0300

    Add project files.

---
+++ Constructor/Baby.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Baby
+    {
+        //Proporties for the Baby class
+        public DateTime BirthTime {  get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+
+        //Default constructor
+        public Baby() {
+
+            BirthTime = DateTime.Now; //Set the birth date current date and time
+            Console.WriteLine("Ingaaaa...\n");
+        }
+
+        //Constructor with parameters for first name and last name
+        public Baby(string _firstName, string _lastName)
+        {
+            BirthTime = DateTime.Now;
+            FirstName = _firstName; //Assign the given values to the properties
+            LastName = _lastName;
+            Console.WriteLine("\n\nIngaaaaa...\n");
+            
+        }
+
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Constructor;
+
+Baby baby1 = new Baby(); //Create a new baby object using the default constuctor
+baby1.FirstName ="Ian";
+baby1.LastName ="Fox";
+Console.WriteLine($"---BABY NUMBER 1---\nNAME: {baby1.FirstName}\nLAST NAME: {baby1.LastName}\nBIRTH TIME: {baby1.BirthTime}");
+
+
+//Crate a new baby obhect using parameterized constructor
+Baby baby2 = new Baby("Jane","Bieber");
+Console.WriteLine($"---BABY NUMBER 2---\nNAME: {baby2.FirstName}\nLAST NAME: {baby2.LastName}\nBIRTH TIME: {baby2.BirthTime}");
